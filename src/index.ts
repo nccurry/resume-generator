89450340e2f0a7fe47d7446c68bbb3c6dd9521bb
ncc <chasecurry@gmail.com>
2021-08-08 10:11:53 -0500

    Work through pug generation

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-'use strict';
-let path = require("path")
-let puppeteer = require("puppeteer")
-let fs = require("fs")
-let yaml = require("js-yaml")
+'use strict'
+import {write} from "fs";
+import {callbackify} from "util";
+
+const path = require("path")
+const puppeteer = require("puppeteer")
+const fs = require("fs")
+const yaml = require("js-yaml")
+const pug = require('pug');
 
 // Parse CLI arguments
 let resumeDataPath: String
@@ -12,32 +16,52 @@ if (!resumeDataPath) {
 }
 
 // Generate html from yaml
+let resumeData: Object
 try {
-  const doc = yaml.load(fs.readFileSync('/home/ixti/example.yml', 'utf8'));
-  console.log(doc);
+  resumeData = yaml.load(fs.readFileSync(resumeDataPath, 'utf8'))
 } catch (e) {
-  console.log(e);
+  console.error('There was a problem parsing ' + resumeDataPath)
+  throw new Error(e)
 }
 
-// https://github.com/puppeteer/puppeteer/blob/v10.2.0/docs/api.md#pagepdfoptions
+const compiledFunction = pug.compileFile(path.join(__dirname, '../templates/template.pug'))
 
-(async() => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(
-    `file:${path.join(__dirname, 'template.html')}`,
-    { waitUntil: 'networkidle0'}
-    );
-    await page.pdf({
-        path: 'test.pdf',
-        format: 'A4',
-        margin: {
-            top: "0px",
-            left: "0px",
-            right: "0px",
-            bottom: "0px"
-        },
-        printBackground: true
-    });
-    await browser.close();
-})();
+let resumeHtml: string
+try {
+  resumeHtml = compiledFunction(resumeData)
+} catch (e) {
+    console.error('There was a problem compiling the template')
+    throw new Error(e)
+}
+
+let generatePdf = function () {
+    console.log('test')
+}
+
+fs.writeFile(path.join(__dirname, '../dist/resume.html'), resumeHtml, generatePdf)
+
+
+// https://github.com/puppeteer/puppeteer/blob/v10.2.0/docs/api.md#pagepdfoptions
+//
+// (async() => {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto(
+//     `file:${path.join(__dirname, 'template.html')}`,
+//     { waitUntil: 'networkidle0'}
+//     );
+//     await page.pdf({
+//         path: 'test.pdf',
+//         format: 'A4',
+//         margin: {
+//             top: "0px",
+//             left: "0px",
+//             right: "0px",
+//             bottom: "0px"
+//         },
+//         printBackground: true
+//     });
+//     await browser.close();
+// })();
+
+export {}
