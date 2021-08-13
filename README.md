8da79236d544d97c27b4d74d0daa7f1f4e7cd239
ncc <chasecurry@gmail.com>
2021-08-13 18:36:59 -0500

    Add hobbies section, tweak spacing.

--- README.md
+++ README.md
@@ -1,9 +1,27 @@
-#
+# Resume Generator
 
-## Prerequisites
+## Generate a resume
 
-```bash
+### Install packages
+```shell
+# Fedora
 sudo dnf module enable nodejs:16
 sudo dnf install module nodejs:16/default
-npm init
+npm install
+```
+
+### Fill out resume data
+
+```shell
+cp exampleData.yaml myResume.yaml
+vi myResume.yaml
+```
+
+### Generate resume
+
+Html and pdf files are output to the [dist](dist) directory.
+
+```shell
+# npx ts-node src/index.ts </path/to/data.yaml> <generate pdf true / false>
+npx ts-node src/index.ts testResume.yaml true
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 'use strict'
+import {PDFOptions} from "puppeteer";
 const path = require("path")
 const puppeteer = require("puppeteer")
 const fs = require("fs")
@@ -8,50 +9,83 @@ const pug = require('pug');
 interface ResumeData {
     name: string
     bannerTitle: string
-    contact: {
-        address?: string
-        encodedAddress?: string
-        email?: string
-        phone?: string
-        website?: string
-        linkedin?: string
+    contactInfo: {
+        value: string
+        faIconClass: string
+        link: string
+    }[]
+    keySkills: {
+        title: string
+        details: string
+    }[]
+    education: {
+        school: string
+        years: string
+        degree: string
+        additionalDetails: string[]
+    }[]
+    certifications: {
+        company: string
+        id: string
+        link: string
+        list: string[]
+    }[]
+    experience: {
+        company: string
+        title: string
+        timeFrame: string
+        details: string[]
     }
-    keySkills: Array<any>
-    education: Array<any>
-    certifications: Array<any>
-    awards: Array<any>
-    [key: string]: any
+    projects: {
+        headline: string
+        additionaDetails: string[]
+    }[]
 }
 
-function parseCliArguments(argv: Array<string>): string | Error {
+interface cliArgs {
+    resumeDataPath: string,
+    generatePdf: boolean
+}
+
+function parseCliArguments(argv: Array<string>): cliArgs | Error {
     let resumeDataPath: string
     resumeDataPath = process.argv[2]
     if (!resumeDataPath) {
         return Error('You must supply the file path to the file containing the resume data.')
     }
-    return resumeDataPath
+
+    let generatePdf: boolean
+    if (generatePdf) {
+        generatePdf = argv[3] === 'true'
+    } else {
+        generatePdf = true
+    }
+
+    return { resumeDataPath, generatePdf }
 }
 
 function getResumeData(filePath: string): ResumeData | Error {
     let resumeData: ResumeData
     try {
-        resumeData = yaml.load(fs.readFileSync(resumeDataPath, 'utf8'))
+        resumeData = yaml.load(fs.readFileSync(filePath, 'utf8'))
     } catch (e) {
         return e
     }
     return resumeData
 }
 
-let resumeDataPath = parseCliArguments(process.argv)
-if (resumeDataPath instanceof Error) {
+// Generate Resume
+
+let args = parseCliArguments(process.argv)
+if (args instanceof Error) {
     console.error('There was a problem parsing the CLI arguments')
-    console.error(resumeDataPath)
+    console.error(args)
     process.exit(1)
 }
 
-let resumeData = getResumeData(resumeDataPath)
+let resumeData = getResumeData(args.resumeDataPath)
 if (resumeData instanceof Error) {
-    console.error('There was a problem parsing the resume data in file ' + resumeDataPath)
+    console.error('There was a problem parsing the resume data in file ' + args.resumeDataPath)
     console.error(resumeData)
     process.exit(1)
 }
@@ -62,12 +96,40 @@ let resumeHtml: string
 try {
   resumeHtml = compiledFunction(resumeData)
 } catch (e) {
-    console.error('There was a problem compiling the template')
+    console.error('There was a problem compiling the resume pug template templates/template.pug')
     throw new Error(e)
 }
 
-let generatePdf = function () {
+let pdfOptions: PDFOptions = {
+    path: `${path.join(__dirname, '../dist/resume.pdf')}`,
+    format: 'a4',
+    margin: {
+        top: "0px",
+        left: "0px",
+        right: "0px",
+        bottom: "0px"
+    },
+    printBackground: true
+}
+
+let generatePdf = function (generate: boolean) {
     // https://github.com/puppeteer/puppeteer/blob/v10.2.0/docs/api.md#pagepdfoptions
+
+    // TODO: Make this pretty
+    // if (generate) {
+    //     try {
+    //         const browser = await puppeteer.launch();
+    //         const page = await browser.newPage();
+    //         await page.goto(
+    //             `file:${path.join(__dirname, '../dist/resume.html')}`,
+    //             { waitUntil: 'networkidle0'}
+    //         );
+    //         await page.pdf(pdfOptions);
+    //         await browser.close();
+    //     } catch (e) {
+    //         return new Error(e)
+    //     }
+    // }
 
     (async() => {
         const browser = await puppeteer.launch();
@@ -76,17 +138,7 @@ let generatePdf = function () {
             `file:${path.join(__dirname, '../dist/resume.html')}`,
             { waitUntil: 'networkidle0'}
         );
-        await page.pdf({
-            path: `${path.join(__dirname, '../dist/resume.pdf')}`,
-            format: 'A4',
-            margin: {
-                top: "0px",
-                left: "0px",
-                right: "0px",
-                bottom: "0px"
-            },
-            printBackground: true
-        });
+        await page.pdf(pdfOptions);
         await browser.close();
     })();
 }
